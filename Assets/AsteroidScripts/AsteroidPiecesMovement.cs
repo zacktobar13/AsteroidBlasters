b1a31b985fcb76a66e485f81c1001fd14981b823
Santos Tobar <zacktobar13@berkeley.edu>
2015-11-11 22:41:27 -0800

    Asteroid behavior stuff

---
+++ Assets/AsteroidScripts/AsteroidPiecesMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidPiecesMovement : MonoBehaviour {
+	bool firstTime = true;
+	void FixedUpdate () {
+		gameObject.transform.Translate(Random.Range(-.18f, .18f), Random.Range(-.18f, .18f), 0f);
+
+		if (firstTime) {
+			StartCoroutine("DestroySelf", .5f);
+			firstTime = false;
+		}
+	}
+
+	IEnumerator DestroySelf(float time) {
+		yield return new WaitForSeconds(time);
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/AsteroidScripts/AsteroidStatusManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidStatusManager : MonoBehaviour {
+
+	public GameObject asteroidPieces;
+
+	public void GetRekt() {
+		Instantiate(asteroidPieces, gameObject.transform.position, gameObject.transform.rotation);
+		Destroy(gameObject);
+	}
+}
