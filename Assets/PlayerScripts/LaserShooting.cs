3463556cbb1a6348a69eb6968afac5d1a12847db
Taylor Moore <tjosephmoore@gmail.com>
2015-11-14 23:31:37 -0600

    p4rticl3z

--- Assets/PlayerScripts/LaserShooting.cs
+++ Assets/PlayerScripts/LaserShooting.cs
@@ -4,9 +4,12 @@ using System.Collections;
 public class LaserShooting : MonoBehaviour {
 
 	public GameObject laser;
+	public GameObject laserParticles;
 	public Transform laserSpawn;
+	public Transform laserParticleSpawn;
 
 	public void FireLaser() {
 		Destroy(Instantiate(laser, laserSpawn.position, laserSpawn.rotation), 4f);
+		Destroy(Instantiate(laserParticles, laserParticleSpawn.position, laserParticleSpawn.rotation), 2f);
 	}
 }
