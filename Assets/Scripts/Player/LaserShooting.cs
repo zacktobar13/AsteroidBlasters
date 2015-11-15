383bee92a7be5d4bea849c142372e034dc6a1448
Taylor Moore <tjosephmoore@gmail.com>
2015-11-15 05:04:25 -0600

    Re-organized everything. Cleaned shit out. Added new menu icons for sound and settings.

---
+++ Assets/Scripts/Player/LaserShooting.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserShooting : MonoBehaviour {
+
+	public GameObject laser;
+	public ParticleSystem laserParticles;
+	public Transform laserSpawn;
+
+	void Start() {
+		laserParticles.Stop();
+	}
+	public void FireLaser() {
+		Destroy(Instantiate(laser, laserSpawn.position, laserSpawn.rotation), 4f);
+		StartCoroutine("ShootParticles");
+	}
+
+	public IEnumerator ShootParticles() {
+		laserParticles.Play();
+		yield return new WaitForSeconds(.3f);
+		laserParticles.Stop();
+	}
+}
