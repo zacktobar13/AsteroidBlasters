2bc1e96604e5034052055ea6d461f5f99bb0325c
Santos Tobar <zacktobar13@berkeley.edu>
2015-11-14 18:24:53 -0800

    barrier changes (still broken)

--- Assets/Barrier/BarrierManager.cs
+++ Assets/Barrier/BarrierManager.cs
@@ -8,14 +8,15 @@ public class BarrierManager : MonoBehaviour {
 
 	/* Change these variables if art for barrier is ever changed */
 	float barrierLength = 1.6f;
-	float respawnTime = 0.105f;
+	float respawnTime = 2.125f;
+	float timeUntilFirstRespawn = 0.119f;
 
 	void OnEnable () {
 		for (int i = 0; i < (topRight.position.x - topLeft.position.x) / barrierLength; i++) {
 			Destroy(Instantiate(barrier, new Vector3(i * barrierLength + topLeft.position.x, topLeft.position.y, 0f), transform.rotation), 3f);
 			Destroy(Instantiate(barrier, new Vector3(i * barrierLength + bottomLeft.position.x, bottomLeft.position.y, 0f), transform.rotation), 3f);
 		}
-		InvokeRepeating("RespawnBarriers", 0f, respawnTime);
+		InvokeRepeating("RespawnBarriers", timeUntilFirstRespawn, respawnTime);
 	}
 
 	void OnDisable() {
@@ -26,7 +27,9 @@ public class BarrierManager : MonoBehaviour {
 	}
 
 	void RespawnBarriers() {
-		Destroy(Instantiate(barrier, new Vector3(topRight.position.x, topLeft.position.y, 0f), transform.rotation), 3f);
-		Destroy(Instantiate(barrier, new Vector3(topRight.position.x, bottomLeft.position.y, 0f), transform.rotation), 3f);
+		for (int i = 0; i < (topRight.position.x - topLeft.position.x) / barrierLength; i++) {
+			Destroy(Instantiate(barrier, new Vector3(i * barrierLength + topRight.position.x, topLeft.position.y, 0f), transform.rotation), 6f);
+			Destroy(Instantiate(barrier, new Vector3(i * barrierLength + topRight.position.x, bottomLeft.position.y, 0f), transform.rotation), 6f);
+		}
 	}
 }
