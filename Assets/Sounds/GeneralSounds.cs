280accb2e3fb3759f1d1c7beef7607ed816b0f00
Santos Tobar <zacktobar13@berkeley.edu>
2015-11-14 20:22:16 -0800

    sound stuff

---
+++ Assets/Sounds/GeneralSounds.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeneralSounds : MonoBehaviour {
+
+	public AudioClip[] Sounds;
+	public SoundManager soundManager;
+	public float volume;
+
+	void Start() {
+		soundManager = GameObject.FindWithTag("SoundManager").GetComponent<SoundManager>();
+		AudioClip sound = Sounds[Random.Range(0, Sounds.Length)];
+
+		if (volume != 0) {
+			soundManager.PlaySound(sound, volume);
+		} else {
+			soundManager.PlaySound(sound);
+		}
+	}
+}
---
+++ Assets/Sounds/SoundManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+	AudioSource audioSources;
+
+	
+	void Start () {
+		audioSources = GetComponent<AudioSource>();
+	}
+	
+	
+	void Update () {
+	
+	}
+
+	public void PlaySound (AudioClip sound, float volume = -1f) {
+		if (volume != -1f) {
+			audioSources.volume = volume;
+		}
+		audioSources.PlayOneShot(sound);
+	}
+}
