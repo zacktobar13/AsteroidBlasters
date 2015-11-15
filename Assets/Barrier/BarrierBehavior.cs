0c8e4a24d4b10e6a06dad1a2c0f5414613c9e72c
Santos Tobar <zacktobar13@berkeley.edu>
2015-11-14 16:43:05 -0800

    Barriers!

---
+++ Assets/Barrier/BarrierBehavior.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarrierBehavior : MonoBehaviour {
+
+	public float speed;
+
+	void Update () {
+		transform.Translate(speed, 0f, 0f);
+	}
+
+	void OnCollisionEnter2D (Collision2D other) {
+		if (other.gameObject.tag == "Player") {
+			other.gameObject.SendMessage("GetRekt");
+		}
+	}
+}
---
+++ Assets/Barrier/BarrierManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarrierManager : MonoBehaviour {
+
+	public GameObject barrier;
+	public Transform bottomLeft, topLeft, topRight;
+
+	/* Change these variables if art for barrier is ever changed */
+	float barrierLength = 1.6f;
+	float respawnTime = 0.1f;
+
+	void OnEnable () {
+		for (int i = 0; i < (topRight.position.x - topLeft.position.x) / barrierLength; i++) {
+			Destroy(Instantiate(barrier, new Vector3(i * barrierLength + topLeft.position.x, topLeft.position.y, 0f), transform.rotation), 3f);
+			Destroy(Instantiate(barrier, new Vector3(i * barrierLength + bottomLeft.position.x, bottomLeft.position.y, 0f), transform.rotation), 3f);
+		}
+		InvokeRepeating("RespawnBarriers", 0f, respawnTime);
+	}
+
+	void OnDisable() {
+		CancelInvoke();
+		foreach (GameObject barrier in GameObject.FindGameObjectsWithTag("Barrier")) {
+			Destroy(barrier);
+		}
+	}
+
+	void RespawnBarriers() {
+		Destroy(Instantiate(barrier, new Vector3(topRight.position.x, topLeft.position.y, 0f), transform.rotation), 3f);
+		Destroy(Instantiate(barrier, new Vector3(topRight.position.x, bottomLeft.position.y, 0f), transform.rotation), 3f);
+	}
+}
