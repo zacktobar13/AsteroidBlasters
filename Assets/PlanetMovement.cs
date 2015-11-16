72db34dbb07c27bef25bb718bb5a9c7db1aba95e
Taylor Moore <tjosephmoore@gmail.com>
2015-11-15 23:30:41 -0600

    frick

---
+++ Assets/PlanetMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetMovement : MonoBehaviour {
+
+	float movementSpeed;
+
+	void Start() {
+		movementSpeed = Random.Range(-10f, -6f);
+	}
+
+	void Update() {
+		transform.Translate(movementSpeed * Time.deltaTime, 0f, 0f);
+	}
+/*
+	void OnBecameInvisible() {
+		Destroy(gameObject);
+	}*/
+}
---
+++ Assets/Scripts/GameManagement/PlanetGenerator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetGenerator : MonoBehaviour {
+	public GameObject[] planets;
+
+	void Start() {
+		InvokeRepeating("SpawnPlanet", 0, Random.Range(1, 10));
+	}
+
+	void SpawnPlanet() {
+		Instantiate(planets[Random.Range(0, planets.Length)], new Vector2(transform.position.x, transform.position.y + Random.Range(-8f, 8f)), transform.rotation);
+	}
+}
